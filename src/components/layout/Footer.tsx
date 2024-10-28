f6416a75d5679883539e6bb287a813eea4520810
alex.blyth <alex.blyth@hedinit.com>
2024-10-28 21:11:32 +0100

    feat: use next link for footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { FaPhone, FaEnvelope, FaLinkedin, FaGithub } from "react-icons/fa";
 import { NAV_LINKS } from "@/constants/links";
 import CTAAnchor from "@/components/buttons/CTAAnchor";
@@ -8,7 +9,7 @@ const Footer = () => {
     <footer id="footer" className="bg-gray-800 text-white pt-6 pb-6">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center md:items-center">
         <div className="flex items-center justify-center md:justify-start flex-1 mb-4 md:mb-0">
-          <a href="#home">
+          <Link href="/#home">
             <Image
               src="/logo.png"
               alt="Logo"
@@ -16,19 +17,19 @@ const Footer = () => {
               height={60}
               className="h-auto object-contain"
             />
-          </a>
+          </Link>
         </div>
 
         <div className="flex flex-1 justify-center items-center mb-8 md:mb-0">
           <div className="flex space-x-6">
             {NAV_LINKS.map((link) => (
-              <a
+              <Link
                 key={link.name}
-                href={link.href}
+                href={`/${link.href}`}
                 className="text-base hover:text-gray-400"
               >
                 {link.name}
-              </a>
+              </Link>
             ))}
             <CTAAnchor />
           </div>
